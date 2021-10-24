b588bf95c34c73ea7bca1f9894e8b6465681c0db
takako0420 <marl0420boro@gmail.com>
2021-10-24 16:23:39 +0900

    ハッシュロケット型の修正

--- app/controllers/calendars_controller.rb
+++ app/controllers/calendars_controller.rb
@@ -1,5 +1,7 @@
 class CalendarsController < ApplicationController
 
+
+
   # １週間のカレンダーと予定が表示されるページ
   def index
     get_week
@@ -39,9 +41,10 @@ class CalendarsController < ApplicationController
       if wday_num >= 7
         wday_num = wday_num -7
       end
-      days = { :month => (@todays_date + x).month, :date => (@todays_date+x).day, :plans => today_plans, :wday => wdays[wday_num]}
+      days = {month: (@todays_date + x).month, date: (@todays_date+x).day, plans: today_plans, wday: wdays[wday_num]}
       @week_days.push(days)
     end
-
   end
 end
+  
+  
